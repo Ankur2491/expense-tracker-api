fbded9aef6dc78a6594858a2c0b895467fa7a54d
Ankur Sharma <ankur.101045@gmail.com>
2023-09-12 13:14:06 +0530

    Add files via upload

---
+++ groceryList.js
@@ -0,0 +1,112 @@
+let groceryList = {
+    "Fruits": {
+        "Mango": "weight",
+        "Banana": "dozen",
+        "Grapes": "weight",
+        "Berries": "weight",
+        "Apples": "weight",
+        "Citrus fruits": "weight",
+        "Peaches": "weight",
+        "Coconut": "unit",
+        "Pears": "weight",
+        "Pineapples": "unit"
+    },
+    "Vegetables": {
+        "Onions": "weight",
+        "Tomato": "weight",
+        "Chilies": "weight",
+        "Ginger": "weight",
+        "Garlic": "weight",
+        "Potato": "weight",
+        "Peppers": "weight",
+        "Broccoli": "weight",
+        "Cauliflower": "weight",
+        "Carrots": "weight"
+    },
+    "Animal Produce": {
+        "Chicken": "weight",
+        "Meat": "weight",
+        "Fish": "weight",
+        "Eggs": "dozen"
+    },
+    "Dairy Products": {
+        "Milk": "litre",
+        "Cheese": "unit",
+        "Butter": "unit",
+        "Yogurt": "unit",
+        "Ghee": "weight"
+    },
+    "Bakery Products": {
+        "Whole wheat bread": "unit",
+        "Bakery buns": "unit",
+        "Biscuits": "unit",
+        "Cakes": "unit"
+    },
+    "Condiments": {
+        "Salt": "weight",
+        "Pepper": "weight",
+        "Sugar": "weight",
+        "Cardamom": "weight",
+        "Cinnamon": "weight",
+        "Nutmeg": "weight",
+        "Cumin": "weight",
+        "Ajwain": "weight",
+        "Turmeric": "weight",
+        "Red Pepper": "weight"
+    },
+    "Oils and Sauces": {
+        "Olive Oil": "litre",
+        "Mustard Oil": "litre",
+        "Sunflower Oil": "litre",
+        "Soy Sauce": "litre",
+        "Vinegar": "litre",
+        "Tomato Ketchup": "litre"
+    },
+    "Grains, Flour and Legumes": {
+        "Atta": "weight",
+        "Rice": "weight",
+        "Pasta": "weight",
+        "Chickpeas": "weight",
+        "Kidney beans": "weight",
+        "Toor dal": "weight",
+        "Chana dal": "weight",
+        "Masoor dal": "weight",
+        "Urad dal": "weight",
+        "Moong dal": "weight",
+        "Black chana": "weight"
+    },
+    "Miscellaneous Snacks": {
+        "Biscuits": "unit",
+        "Chips": "unit",
+        "Walnuts": "weight",
+        "Almonds": "weight",
+        "Raisins": "weight",
+        "papad": "unit",
+        "Honey": "litre"
+    },
+    "Cleaning Supplies": {
+        "Dishwasher soap": "unit",
+        "Detergent powder": "weight",
+        "Shaving cream": "unit",
+        "Toilet cleaner": "unit",
+        "Floor cleaner": "unit",
+        "Soap": "unit",
+        "Shampoo": "unit",
+        "Tissue paper": "unit",
+        "Toothpaste": "unit",
+        "Toothbrush": "unit",
+        "Broom": "unit",
+        "Mopper": "unit"
+    },
+    "Pooja Items": {
+        "Ghee": "unit",
+        "Agarbatti": "unit",
+        "Dhoop": "unit"
+    },
+    "Other Items": {
+        "Garbage bags": "unit",
+        "Room fresheners": "unit",
+        "Mosquito repellents": "unit"
+    }
+}
+module.exports = {groceryList}
